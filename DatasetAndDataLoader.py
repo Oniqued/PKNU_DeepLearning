260dfdf487f5315078e952ebc0f04eee8566243a
Oniqued <zktl3575@gmail.com>
2023-04-01 18:49:36 +0900

    데이터셋을 순회하고 시각화하기

--- DatasetAndDataLoader.py
+++ DatasetAndDataLoader.py
@@ -4,7 +4,7 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
-
+# 데이터셋 불러오기
 training_data = datasets.FashionMNIST(
     root="data",
     train=True,
@@ -18,4 +18,29 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor()
 )
+
+# 데이터셋을 순회하고 시각화하기
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+
+figure = plt.figure(figsize=(8,8))
+cols, rows = 3,3
+for i in range(1,cols*rows+1):
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray") # squeeze()는 shape이 (1,28,28) 텐서를 (28,28)인 텐서로 변환
+plt.show()
 
