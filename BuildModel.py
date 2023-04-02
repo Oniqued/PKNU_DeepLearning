40b612e4604e8dfe372ae6a46f42611cd7085892
Oniqued <zktl3575@gmail.com>
2023-04-02 15:53:20 +0900

    인스턴스를 생성하고, device로 이동한 뒤, 구조를 출력

--- BuildModel.py
+++ BuildModel.py
@@ -31,3 +31,22 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
+# 유사한 구조의 네트워크를 다르게 정의할 수도 있다.
+class NeuralNetwork2(nn.Module):  # 512와 1024 차이 뿐 다른건 같은 코드
+    def __init__(self):
+        super(NeuralNetwork2, self).__init__()
+        self.fc1 = nn.Linear(img_height * img_width * num_channels, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)  # flatten the input image / x.size -> batch size / -1 -> undefined // 그냥 이런것도 있다
+        out = self.relu(self.fc1(x))
+        out = self.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+# NeuralNetwork의 인스턴스를 생성하고 이를 device로 이동한 뒤, 구조를 출력한다.
+model = NeuralNetwork().to(device) # 필요한 작업... CPU 쓸건지 GPU 쓸건지
+print(model)
