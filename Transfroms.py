33c1813f4ead386e38b60718cb79f1de803baee6
Oniqued <zktl3575@gmail.com>
2023-04-02 15:18:08 +0900

    numpy 배열 형태의 이미지를 Tensor로 변환 및 정규화

--- Transfroms.py
+++ Transfroms.py
@@ -217,3 +217,83 @@ for i, tsfrm in enumerate([scale, crop, composed]):
     show_landmarks(**transformed_sample)
 
 plt.show()
+
+# numpy 배열 형태의 이미지를 Tensor로 변환하고 정규화 하는 코드
+class MyToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __init__(self):
+        self.img_tensorfier = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])  # Normalize가 하는 일은 무엇인가
+
+    def __call__(self, sample):
+        image, landmarks = sample['image'], sample['landmarks']
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C x H x W
+        image = self.img_tensorfier(image)
+
+        # image = image.transpose((2, 0, 1))
+        return {'image': image,  # torch.from_numpy(image),
+                'landmarks': torch.from_numpy(landmarks)}
+
+transformed_dataset = FaceLandmarksDataset(csv_file=data_dir + '/face_landmarks.csv',
+                                           root_dir=data_dir,
+                                           transform=transforms.Compose([
+                                               Rescale((256, 256)),
+                                               RandomCrop((224, 224)),
+                                               MyToTensor()
+                                           ]))
+
+for i in range(len(transformed_dataset)):
+    sample = transformed_dataset[i]
+
+    print(i, sample['image'].size(), sample['landmarks'].size())
+    print(sample['image'].min(), sample['image'].max())
+
+    if i == 3:
+        break
+
+dataloader = DataLoader(transformed_dataset, batch_size=4,
+                        shuffle=True, num_workers=0)
+
+
+# Helper function to show a batch
+def show_landmarks_batch(sample_batched):
+    """Show image with landmarks for a batch of samples."""
+    images_batch, landmarks_batch = \
+            sample_batched['image'], sample_batched['landmarks']
+    batch_size = len(images_batch)
+    im_size = images_batch.size(2)
+    grid_border_size = 2
+
+    grid = utils.make_grid(images_batch)
+    grid = grid / 2 + 0.5
+    plt.imshow(grid.numpy().transpose((1, 2, 0)))
+
+    for i in range(batch_size):
+        plt.scatter(landmarks_batch[i, :, 0].numpy() + i * im_size + (i + 1) * grid_border_size,
+                    landmarks_batch[i, :, 1].numpy() + grid_border_size,
+                    s=10, marker='.', c='r')
+
+        plt.title('Batch from dataloader')
+
+# if you are using Windows, uncomment the next line and indent the for loop.
+# you might need to go back and change "num_workers" to 0.
+
+# if __name__ == '__main__':
+for i_batch, sample_batched in enumerate(dataloader):
+    print(i_batch, sample_batched['image'].size(),
+          sample_batched['landmarks'].size())
+
+    # observe 4th batch and stop.
+    if i_batch == 3:
+        plt.figure()
+        # show_landmarks(sample_batched['image'][0].numpy().transpose((1, 2, 0)), sample_batched['landmarks'][0])
+        show_landmarks_batch(sample_batched)
+        plt.axis('off')
+        plt.ioff()
+        plt.show()
+        break
